6d62fc70694e674c5393c5a810e221898b2b412f
Diana Pajon <DPajon.ITR@supervielle.com.ar>
2023-08-20 21:52:25 -0300

    Commit inicial del repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarContactoComponent } from './contactos/editar-contacto/editar-contacto.component';
+import { ListaContactosComponent } from './contactos/lista-contactos/lista-contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:ListaContactosComponent, data:{animation:'enterLeavePage'}},
+  {path:"contacto", component:EditarContactoComponent, data:{animation:'enterLeavePage'}},
+  {path:"contacto/:id", component:EditarContactoComponent, data:{animation:'enterLeavePage'}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
